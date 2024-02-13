3db0b53e9aa24dae4342d45d0c1b731b4bd7bfd9
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-13 18:38:46 -0300

    Add placeholder images

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 console.log('hi');
+
+// Placeholder images
+const imgElements = document.querySelectorAll('img');
+const placeholderImageUrl = 'https://picsum.photos/300/300';
+imgElements.forEach(img => {
+    img.src = placeholderImageUrl;
+});
