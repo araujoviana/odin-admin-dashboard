e72ce7114a7951a0ef5d7907e6f7bb962971aacd
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-02-13 22:17:14 -0300

    Add dashboard SVGs minus tasks

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 console.log('hi');
 
 // Placeholder images
-const imgElements = document.querySelectorAll('img');
-const placeholderImageUrl = 'https://picsum.photos/100/100';
-imgElements.forEach(img => {
-    img.src = placeholderImageUrl;
-});
+// const imgElements = document.querySelectorAll('img');
+// const placeholderImageUrl = 'https://picsum.photos/100/100';
+// imgElements.forEach(img => {
+//    img.src = placeholderImageUrl;
+// });
+
+// TODO: Placing multiple images dinamically
